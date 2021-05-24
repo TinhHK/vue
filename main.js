d76f8a7246dcd5e7edf0ee13787f6b46262707d3
Hà Khắc Tình - CMC Global DU18 <hktinh@ldap.cmcglobal.com.vn>
2021-05-24 15:17:16 +0700

    Event handling

--- main.js
+++ main.js
@@ -10,16 +10,33 @@ var app = new Vue({
         variants: [
             {
                 variantID: 123,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: './images/funnya.jpg'
             },
             {
                 variantID: 124,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: './images/funnya1.jpg'
             },
             {
                 variantID: 125,
-                variantColor: 'red'
+                variantColor: 'red',
+                variantImage: './images/funnya2.jpg'
             }
         ],
+        cart: 0,
+    },
+    methods: {
+        addToCard: function() {
+            this.cart += 1;
+        },
+        removeCard() {
+            if(this.cart > 0) {
+                this.cart -= 1;
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
     },
 });
