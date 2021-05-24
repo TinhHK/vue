89e12c9f6b62ac4cfa9c4fd5ea5f41a64f6a973a
Hà Khắc Tình - CMC Global DU18 <hktinh@ldap.cmcglobal.com.vn>
2021-05-24 09:56:31 +0700

    for loop

--- main.js
+++ main.js
@@ -3,8 +3,23 @@ var app = new Vue({
     data: {
         product: 'Angel 1',
         image: './images/girl.jpg',
-        imageStyle: 'max-width:100%; height:100px;',
+        sexyGirl: 'a sexy girl',
         inventory: 101,
         best: false,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantID: 123,
+                variantColor: 'blue'
+            },
+            {
+                variantID: 124,
+                variantColor: 'green'
+            },
+            {
+                variantID: 125,
+                variantColor: 'red'
+            }
+        ],
     },
 });
